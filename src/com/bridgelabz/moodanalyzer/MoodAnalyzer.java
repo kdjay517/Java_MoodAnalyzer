54bd624e83c2ece258e2aa38c85bd0cb5ae5f1ec
kdjay517 <kdjay517@gmail.com>
2021-11-30 21:26:11 +0530

    Added: when there is no message given it is raised by null pointer exception and handled by try catch blocks

--- src/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
+++ src/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
@@ -1,25 +1,30 @@
 package com.bridgelabz.moodanalyzer;
 
 public class MoodAnalyzer {
-	
+
 	private String message;
-	
+
 	public MoodAnalyzer() {
-		
+
 	}
-	
+
 	public MoodAnalyzer(String message) {
 		this.message = message;
 	}
-	
+
 	public void setMessage(String message) {
 		this.message = message;
 	}
 
 	public String readMessage() {
-		if (message.toLowerCase().contains("happy")) {
-			return "HAPPY";
+		try {
+			if (message.toLowerCase().contains("sad")) {
+				return "SAD";
+			} else {
+				return "HAPPY";
+			}
+		} catch (NullPointerException e) {
+			return "Exception Handled";
 		}
-			return null;	
 	}
 }
--- src/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
+++ src/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
@@ -15,11 +15,26 @@ public class MoodAnalyzerTest {
 	}
 	
 	@Test
-
 	public void givenMessage_WhenSad_ShouldReturnSad() {
+		moodAnalyzer.setMessage("Iam in Sad Mood");
+		String result = moodAnalyzer.readMessage();
+		Assert.assertEquals("SAD",result);
+		System.out.println("Iam in Sad Mood");
+	}
+	
+	@Test
+	public void givenMessage_WhenHappy_ShouldReturnHappy() {
 		moodAnalyzer.setMessage("Iam in Happy Mood");
 		String result = moodAnalyzer.readMessage();
 		Assert.assertEquals("HAPPY",result);
 		System.out.println("Iam in Happy Mood");
+	}
+	
+	@Test
+	public void givenMessage_Null_ShouldReturnExceptionHandled() {
+		moodAnalyzer.setMessage(null);
+		String result = moodAnalyzer.readMessage();
+		Assert.assertEquals("Exception Handled",result);
+		System.out.println("Message should not be Empty");
 	}
 }
